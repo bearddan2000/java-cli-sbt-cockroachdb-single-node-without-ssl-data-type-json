77afa0459cace21f3b6109317beda41e7097fd81
bearddan2000 <bearddan2000@yahoo.com>
2022-10-08 21:55:10 -0400

    initial commit

---
+++ db/sql/01-table-insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (id, breed_color)
+VALUES
+(1, '{ "breed": "Am Bulldog", "color": "White"}'),
+(2, '{ "breed": "Blue Tick", "color": "Grey"}'),
+(3, '{ "breed": "Labrador", "color": "Black"}'),
+(4, '{ "breed": "Gr Shepard", "color": "Brown"}');
